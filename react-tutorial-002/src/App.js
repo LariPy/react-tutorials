3434d3d75221b79324cb700123220e7ef2cdb64f
Pyykkö Lari 20955 <lari.pyykko@edu.riveria.fi>
2022-12-21 13:05:28 +0200

    21.12.2022

--- react-tutorial-002/src/App.js
+++ react-tutorial-002/src/App.js
@@ -12,6 +12,8 @@ import { useState, useEffect } from 'react'
 import { format } from 'date-fns'
 import api from './api/posts'
 import useWindowSize from './hooks/useWindowSize'
+import useAxiosFetch from './hooks/useAxiosFetch'
+import { DataProvider } from './context/DataContext'
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -24,25 +26,11 @@ function App() {
   const navigate = useNavigate();
   const { width } = useWindowSize()
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts')
-        setPosts(response.data)
-      } catch (err) {
-        if (err.response) {
-        // Not in the 200 response range
-        console.log(err.response.data)
-        console.log(err.response.status)
-        console.log(err.response.headers)
-        } else {
-          console.log(`Error: ${err.message}`)
-        }
-      }
-    }
+  const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts')
 
-    fetchPosts()
-  }, [])
+  useEffect(() => {
+    setPosts(data)
+  },[data])
 
   useEffect(() => {
     const filteredResults = posts.filter(post => 
@@ -97,38 +85,43 @@ function App() {
 
   return (
     <div className="App">
-      <Header title='React JS Blog' width={width}/>
-      <Nav search={search} setSearch={setSearch}/>
-      
-      <Routes>
-        <Route path='/'
-        element={<Home posts={searchResults}/>} />
-        <Route path='/post'
-        element={<NewPost
-          handleSubmit={handleSubmit}
-          postTitle={postTitle}
-          setPostTitle={setPostTitle}
-          postBody={postBody}
-          setPostBody={setPostBody}
-        />} />
-         <Route path='/edit/:id'
-        element={<EditPost
-          posts={posts}
-          handleEdit={handleEdit}
-          editTitle={editTitle}
-          setEditTitle={setEditTitle}
-          editBody={editBody}
-          setEditBody={setEditBody}
-        />} />
-        <Route path='/post/:id'
-        element={<PostPage posts={posts} handleDelete={handleDelete} />} />
-        <Route path='/about'
-        element={<About />} />
-        <Route path='*'
-        element={<Missing />} />
-      </Routes>
-      
-      <Footer />
+      <DataProvider>
+        <Header title='React JS Blog' width={width}/>
+        <Nav search={search} setSearch={setSearch}/>
+        
+        <Routes>
+          <Route path='/'
+          element={<Home posts={searchResults}/>}
+          fetchError={fetchError}
+          isLoading={isLoading}
+          />
+          <Route path='/post'
+          element={<NewPost
+            handleSubmit={handleSubmit}
+            postTitle={postTitle}
+            setPostTitle={setPostTitle}
+            postBody={postBody}
+            setPostBody={setPostBody}
+          />} />
+          <Route path='/edit/:id'
+          element={<EditPost
+            posts={posts}
+            handleEdit={handleEdit}
+            editTitle={editTitle}
+            setEditTitle={setEditTitle}
+            editBody={editBody}
+            setEditBody={setEditBody}
+          />} />
+          <Route path='/post/:id'
+          element={<PostPage posts={posts} handleDelete={handleDelete} />} />
+          <Route path='/about'
+          element={<About />} />
+          <Route path='*'
+          element={<Missing />} />
+        </Routes>
+        
+        <Footer />
+      </DataProvider>
     </div>
   );
 }
