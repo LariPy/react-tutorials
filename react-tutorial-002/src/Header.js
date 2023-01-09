c18fce98c98df3fa2565f1c826f9a664e4d9399a
Pyykkö Lari 20955 <lari.pyykko@edu.riveria.fi>
2023-01-09 14:15:25 +0200

    9.1.2023

--- react-tutorial-002/src/Header.js
+++ react-tutorial-002/src/Header.js
@@ -1,15 +1,17 @@
-import { Link } from 'react-router-dom'
-import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa'
+import { FaLaptop, FaTabletAlt, FaMobileAlt } from 'react-icons/fa';
+import useWindowSize from './hooks/useWindowSize';
 
-const Header = ({ title, width }) => {
-  return (
-    <header className="Header">
-        <h1><Link to='/' style={{ color: 'inherit', textDecoration: 'inherit' }} >{title}</Link></h1>
-        {width < 768 ? <FaMobileAlt />
-          : width < 992 ? <FaTabletAlt />
-          : <FaLaptop />}
-    </header>
-  )
+const Header = ({ title }) => {
+    const { width } = useWindowSize();
+
+    return (
+        <header className="Header">
+            <h1>{title}</h1>
+            {width < 768 ? <FaMobileAlt />
+                : width < 992 ? <FaTabletAlt />
+                    : <FaLaptop />}
+        </header>
+    )
 }
 
 export default Header
